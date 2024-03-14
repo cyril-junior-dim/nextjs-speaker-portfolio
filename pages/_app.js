5f1e75bc6fce50bdc60eebca07f1f755864079f9
cyril-junior-dim <37346841+cyril-junior-dim@users.noreply.github.com>
2024-03-14 14:41:39 +0100

    Updated PayPal ScriptProvider location

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,13 @@
 import "../styles/globals.css";
 import { LocationContextProvider } from "../security/helpers";
 import { Footer } from "../components/";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PayPalScriptProvider
-      options={{
-        "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
-        currency: "EUR",
-        components: "buttons",
-      }}
-    >
-      <LocationContextProvider>
-        <Component {...pageProps} />
-        <Footer />
-      </LocationContextProvider>
-    </PayPalScriptProvider>
+    <LocationContextProvider>
+      <Component {...pageProps} />
+      <Footer />
+    </LocationContextProvider>
   );
 }
 
--- pages/bookings/coaching/checkout/index.js
+++ pages/bookings/coaching/checkout/index.js
@@ -1,78 +1,104 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router'
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import { RiQuestionLine } from "react-icons/ri";
-import { useInvalidUrlRoutes } from '../../../../security/helpers';
-import { Navbar, Product, PayPalCheckoutButton, Popup } from '../../../../components';
-import products from '../../../../data/product-data';
-import styles from '../../../../styles/pages/checkout.module.css';
+import { useInvalidUrlRoutes } from "../../../../security/helpers";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import {
+  Navbar,
+  Product,
+  PayPalCheckoutButton,
+  Popup,
+} from "../../../../components";
+import products from "../../../../data/product-data";
+import styles from "../../../../styles/pages/checkout.module.css";
 import Head from "next/head";
 
 const Checkout = () => {
-    useInvalidUrlRoutes();
+  useInvalidUrlRoutes();
 
-    const router = useRouter();
-    const [buttonPopup, setButtonPopup] = useState(true);
+  const router = useRouter();
+  const [buttonPopup, setButtonPopup] = useState(true);
 
-    try{
-        const product = products[router.query.product];
+  try {
+    const product = products[router.query.product];
 
-        return(
-            <div className={styles.checkout}>
-                <Head>
-                    <title>Checkout</title>
-                    <meta name="description" content="Coaching Checkout"/>
-                    <meta property="og:title" content="Checkout"/>
-                    <meta property="og:description" content="Coaching checkout"/>
-                    <meta property="og:url" content="https://cyriljuniordim.com/bookings/coaching/checkout"/>
-                    <meta property="og:type" content="website"/>
-                    <link rel="icon" href="/assets/logos/logo-white.png" />
-                </Head>
-                <div className={styles.checkout}>
-                    <Navbar />
-                </div>
-                <div className={styles.checkoutDetailsContainer}>
-                    <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                        <h2>Instructions</h2>
-                        <>{product.instructions}</>
-                    </Popup>
-                    <div className={`${styles.checkoutDetails} section__padding`}>
-                        <div className={styles.checkoutHelp}>
-                            <RiQuestionLine color="#000000" size={32} onClick={() => setButtonPopup(true)}/>
-                        </div>
-                        <Product
-                            title={product.title}
-                            description={product.description}
-                            price={"€" + product.price}
-                            img={product.img}
-                        />
-                        <div className={styles.paypalButtonContainer}>
-                            <PayPalCheckoutButton product={product} />
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div className={styles.checkout}>
+        <Head>
+          <title>Checkout</title>
+          <meta name="description" content="Coaching Checkout" />
+          <meta property="og:title" content="Checkout" />
+          <meta property="og:description" content="Coaching checkout" />
+          <meta
+            property="og:url"
+            content="https://cyriljuniordim.com/bookings/coaching/checkout"
+          />
+          <meta property="og:type" content="website" />
+          <link rel="icon" href="/assets/logos/logo-white.png" />
+        </Head>
+        <div className={styles.checkout}>
+          <Navbar />
+        </div>
+        <div className={styles.checkoutDetailsContainer}>
+          <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+            <h2>Instructions</h2>
+            <>{product.instructions}</>
+          </Popup>
+          <div className={`${styles.checkoutDetails} section__padding`}>
+            <div className={styles.checkoutHelp}>
+              <RiQuestionLine
+                color="#000000"
+                size={32}
+                onClick={() => setButtonPopup(true)}
+              />
             </div>
-        );
-    } catch (e) {
-        console.log(e.message)
-        return (
-            <div>
-                <div>
-                    <Navbar />
-                </div>
-                <div style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    height: "80vh"
-                }}>
-                    <h1 style={{
-                        textAlign: "center",
-                        margin: "auto"
-                    }}>500 | Internal Server Error</h1>
-                </div>
+            <Product
+              title={product.title}
+              description={product.description}
+              price={"€" + product.price}
+              img={product.img}
+            />
+            <div className={styles.paypalButtonContainer}>
+              <PayPalScriptProvider
+                options={{
+                  "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+                  currency: "EUR",
+                  components: "buttons",
+                }}
+              >
+                <PayPalCheckoutButton product={product} />{" "}
+              </PayPalScriptProvider>
             </div>
-        )
-    }
-
+          </div>
+        </div>
+      </div>
+    );
+  } catch (e) {
+    console.log(e.message);
+    return (
+      <div>
+        <div>
+          <Navbar />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "80vh",
+          }}
+        >
+          <h1
+            style={{
+              textAlign: "center",
+              margin: "auto",
+            }}
+          >
+            500 | Internal Server Error
+          </h1>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Checkout;
