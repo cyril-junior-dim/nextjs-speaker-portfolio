c425a972df4316fa9f9c01aacbf82a6dea56dd2e
cyril-junior-dim <37346841+cyril-junior-dim@users.noreply.github.com>
2024-03-14 10:40:42 +0100

    Updated PayPal link and upgradeed NextJs

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,22 @@
-import '../styles/globals.css'
-import { LocationContextProvider } from '../security/helpers';
-import { Footer } from '../components/';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import "../styles/globals.css";
+import { LocationContextProvider } from "../security/helpers";
+import { Footer } from "../components/";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <PayPalScriptProvider
-          options={{
-              "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
-              currency: "EUR"
-          }}
-      >
-          <LocationContextProvider>
-              <Component {...pageProps} />
-              <Footer />
-          </LocationContextProvider>
-      </PayPalScriptProvider>
+    <PayPalScriptProvider
+      options={{
+        "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+        currency: "EUR",
+      }}
+    >
+      <LocationContextProvider>
+        <Component {...pageProps} />
+        <Footer />
+      </LocationContextProvider>
+    </PayPalScriptProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
